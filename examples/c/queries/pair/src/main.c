1eddaba2558f30d9cbfb416ca6946d4498ac1a52
raojm <raojm@91ne.com>
2023-02-17 15:45:32 +0800

    change query pair example

--- examples/c/queries/pair/src/main.c
+++ examples/c/queries/pair/src/main.c
@@ -8,12 +8,25 @@ typedef struct {
     double y;
 } Position;
 
+ECS_STRUCT(MassEntityUniqueID, { 
+    uint64_t UniqueID; 
+});
+
 ECS_STRUCT(RoomUniqueID, {
     uint16_t ID;
 });
 
 ECS_STRUCT(TableUniqueID, {
     uint16_t ID;
+});
+
+ECS_STRUCT(TestComponent1, {
+    uint32_t ID1;
+});
+
+ECS_STRUCT(TestComponent2, {
+    uint32_t ID1;
+    uint32_t ID2;
 });
 
 void Trigger(ecs_iter_t *it) {
@@ -36,10 +49,12 @@ void Trigger(ecs_iter_t *it) {
 int main(int argc, char *argv[]) {
     g_pstFLECSWorld = ecs_init_w_args(argc, argv);
 
-    
-
+    ECS_META_COMPONENT(g_pstFLECSWorld, MassEntityUniqueID);
     ECS_META_COMPONENT(g_pstFLECSWorld, RoomUniqueID);
     ECS_META_COMPONENT(g_pstFLECSWorld, TableUniqueID);
+
+    ECS_META_COMPONENT(g_pstFLECSWorld, TestComponent1);
+    ECS_META_COMPONENT(g_pstFLECSWorld, TestComponent2);
 
     ECS_TAG(g_pstFLECSWorld, GameRoomTag);
     ECS_TAG(g_pstFLECSWorld, GameTableTag);
@@ -49,7 +64,7 @@ int main(int argc, char *argv[]) {
 
     // ecs_add(g_pstFLECSWorld, EcsWorld, RoomUniqueID);
 
-    ecs_entity_t t_any = ecs_observer_init(g_pstFLECSWorld, &(ecs_observer_desc_t){
+    ecs_entity_t stObserverOnSetSystem = ecs_observer_init(g_pstFLECSWorld, &(ecs_observer_desc_t){
         .filter.terms[0].id = MyTestTag1,
         .filter.terms[1].id = EcsWildcard,
         .events = {EcsOnSet},
@@ -57,6 +72,19 @@ int main(int argc, char *argv[]) {
         // .ctx = ctx_any
     });
 
+    assert(stObserverOnSetSystem != 0);
+
+    MassEntityUniqueID stPlayerRaojm = {.UniqueID = 100001};
+
+    ecs_entity_t Player001 = ecs_entity_init(g_pstFLECSWorld, &(ecs_entity_desc_t){ .name = "Player001" });
+    ecs_set_ptr(g_pstFLECSWorld, Player001, MassEntityUniqueID, &stPlayerRaojm);
+
+    ecs_set_pair(g_pstFLECSWorld, Player001, TestComponent1, ecs_id(TestComponent2), {.ID1 = 100} );
+    ecs_set_pair_second(g_pstFLECSWorld, Player001, MyTestTag1, TestComponent2, {.ID1 = 200, .ID2=300} );
+
+
+    ecs_entity_t stPlayerPairDataEntity = ecs_entity_init(g_pstFLECSWorld, &(ecs_entity_desc_t){ .name = "PlayerPairDataEntity" });
+    ecs_add_pair(g_pstFLECSWorld, stPlayerPairDataEntity, EcsChildOf, Player001);
 
     RoomUniqueID stRoomUniqueID0001 = {.ID=0x0001};
     RoomUniqueID stRoomUniqueID0002 = {.ID=0x0002};
@@ -70,9 +98,12 @@ int main(int argc, char *argv[]) {
     TableUniqueID stTableUniqueID0006 = {.ID=0x0006};
 
     // ecs_new_entity(ecs, "Sun");
-    ecs_entity_t GameRoom0001 = ecs_entity_init(g_pstFLECSWorld, &(ecs_entity_desc_t){ .name = "GameRoom0001", .add = { GameRoomTag,  MyTestTag1} });
+    //ecs_entity_init 会先add 再set name, 触发EcsOnSet时entity name会还是空
+    ecs_entity_t GameRoom0001 = ecs_entity_init(g_pstFLECSWorld, &(ecs_entity_desc_t){ .name = "GameRoom0001" });
+    //通过.id 修改己存在的entity, 可以一次添加多个组件 节省创建多余的table类型
+    ecs_entity_init(g_pstFLECSWorld, &(ecs_entity_desc_t){ .id = GameRoom0001, .add = { GameRoomTag,  MyTestTag1, MyTestTag2} });
     // ecs_set_id(g_pstFLECSWorld, GameRoom0001, MyTestTag2, 0, NULL);
-    ecs_add_id(g_pstFLECSWorld, GameRoom0001, MyTestTag2);
+    // ecs_add_id(g_pstFLECSWorld, GameRoom0001, MyTestTag2);
     ecs_set_ptr(g_pstFLECSWorld, GameRoom0001, RoomUniqueID, &stRoomUniqueID0001);
     ecs_entity_t GameRoom0002 = ecs_entity_init(g_pstFLECSWorld, &(ecs_entity_desc_t){ .name = "GameRoom0002", .add = { GameRoomTag } });
     ecs_set_ptr(g_pstFLECSWorld, GameRoom0002, RoomUniqueID, &stRoomUniqueID0002);
@@ -103,8 +134,10 @@ int main(int argc, char *argv[]) {
     ecs_query_t *pstQuery = ecs_query(g_pstFLECSWorld, {
         .filter.terms = {
             // Read from entity's Local position
-            { .id = GameRoomTag, .src.flags = EcsParent }, 
-            // { .id = ecs_pair(EcsChildOf, GameRoom0001)},
+            // { .id = GameRoomTag, .src.flags = EcsParent }, 
+            { .id = ecs_pair(EcsChildOf, GameRoom0001)},
+            // { .id = ecs_pair(EcsChildOf, GameRoomTag)}
+            // { .id = ecs_pair(EcsChildOf, GameRoom0002) }
         }
     });
 
@@ -112,6 +145,16 @@ int main(int argc, char *argv[]) {
     ecs_iter_t it = ecs_query_iter(g_pstFLECSWorld, pstQuery);
     while (ecs_query_next(&it)) {
 
+        ecs_id_t field1 = ecs_field_id(&it, 1);
+        auto firstPair = ecs_pair_first(g_pstFLECSWorld, field1);
+
+        auto secondPair = ecs_pair_second(g_pstFLECSWorld, field1);
+
+        printf("query field1 id: %llu, firstPair: %llu, second: %llu  \n", field1, firstPair, secondPair);
+        // printf("query field1 id: %llu , name:%s \n", field1, ecs_get_name(g_pstFLECSWorld, field1));
+
+        
+        
         for (int i = 0; i < it.count; i ++) {
             printf("entity name: %s\n", ecs_get_name(g_pstFLECSWorld, it.entities[i]));
         }
